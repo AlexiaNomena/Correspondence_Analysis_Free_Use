3438157393d1dff89cafd2a667ee2b00271e6575
AlexiaNomena <missralexia1@gmail.com>
2021-08-02 13:57:56 +0200

    improved

--- Methods/mcmca.py
+++ Methods/mcmca.py
@@ -161,15 +161,6 @@ def MCMCA(Data, row_vals, col_vals, rows_to_Annot, cols_to_Annot, Label_rows, La
             Coords_rows = Fact["Factors_rows"].copy()
             Coords_cols = Fact["Factors_columns"].copy()
             Inertia = Fact["Inertia"].copy()
-            """
-            if rows_to_Annot is not None:
-                annot_rows = rows_to_Annot
-                rows_to_Annot = []
-                for s in range(len(annot_rows)):
-                    ind = np.where(Cont.index == annot_rows[s])[0]
-                    if len(ind) >= 1: # row should appear only one time
-                        rows_to_Annot = rows_to_Annot + list(ind)
-            """
             
             fig, xy_rows, xy_cols = Display(Coords_rows, Coords_cols, Inertia, Cont, rows_to_Annot, cols_to_Annot, Label_rows, Label_cols, 
                                   markers, col, figtitle, outliers, dtp=dtp,
